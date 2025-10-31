a0623a1230949d0a27ce809362f78b576ee65c2c
quackem2 <quackem2@my.erau.edu>
2025-10-30 21:28:18 -0700

    created mutuals.py. added pseudocode for planning
    Implement logic to read usernames from a file and find mutual followers.

---
+++ mutuals.py
@@ -0,0 +1,11 @@
+//get username from rust
+
+//open userdata for reading
+
+//read each line of file, checking first character for the username provided
+  //if username in file
+    // read lines into followers and following
+    // generate sets for each
+    // find intersection between them 
+    // make new list with those usernames inside it.
+    // send list back to rust
